dc6f8fc4692247aa44b2fd9672a5b162b0dc1848
Greg Benner <gregbenner@gmail.com>
2013-06-16 16:35:08 +0200

    first

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/github/"
+http_images_path = "/github/images"
+http_fonts_path = "/github/fonts"
+css_dir = "public/github/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
